4e636dbd6df7641961637f2bdfcc9b9ee19f1f2a
liubo0501 <1187448504@qq.com>
2018-01-26 09:26:36 +0800

    redis相关

--- src/main/java/com/controller/MyController.java
+++ src/main/java/com/controller/MyController.java
@@ -1,9 +1,16 @@
 package com.controller;
 
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +19,7 @@ import com.entity.Robot;
 import com.entity.User;
 import com.mapper.RobotMapper;
 import com.mapper.UserMapper;
+import com.redis.RedisClient;
 import com.service.MyService;
 
 @RestController
@@ -25,6 +33,10 @@ public class MyController {
 	private RobotMapper robotMapper;
 	@Autowired
 	private MyService service;
+	@Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Autowired  
+	private RedisClient redisClient;  
 	
     @RequestMapping("/hello1")
     public String Hello(){
@@ -37,13 +49,23 @@ public class MyController {
     	User user = userMapper.getUserById(1);
     	user.setName("管理员123");
     	userMapper.update(1, user);
-    	System.out.println(user.getUsername());
+    	logger.error(user.getUsername());
+    	
+    	stringRedisTemplate.opsForValue().set("username", user.getUsername());
+    	
+    	logger.error("aaaaa   " + stringRedisTemplate.opsForValue().get("username"));
     	
     	service.hello();
 //       List<Robot> map = robotMapper.getRobotList();
        Robot map = robotMapper.getRobotById("17WR201801");
-       System.out.println(JSON.toJSONString(map));
-        return "Hello World";
+       try {
+		redisClient.set("map",JSON.toJSONString(map) );
+	} catch (Exception e) {
+		e.printStackTrace();
+	}  
+      
+       logger.error(redisClient.get("map"));
+       return "Hello World";
     }
     
     @Autowired
@@ -53,5 +75,27 @@ public class MyController {
     public String sample(){
         return "spring boot success ! and profile is ==>"+
         env.getProperty("server.port")+"=====>";
+    }
+    
+    
+    @RequestMapping("/uid")
+    String uid(HttpSession session) {
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+        return session.getId();
+    }
+    
+    @GetMapping("/index")
+    public ResponseEntity index(HttpSession httpSession) {
+        httpSession.setAttribute("user", "helloword123");
+        return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping("/helloword")
+    public ResponseEntity hello(HttpSession httpSession) {
+        return ResponseEntity.ok(httpSession.getAttribute("user"));
     }
 }
