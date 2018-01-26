3974a927a25db9285089a7a5dfb2cac64800a0ce
liubo0501 <1187448504@qq.com>
2018-01-26 14:16:03 +0800

    config文件位置同一放置

--- src/main/java/com/config/SessionConfig.java
+++ src/main/java/com/config/SessionConfig.java
@@ -1,4 +1,4 @@
-package com.util;
+package com.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
